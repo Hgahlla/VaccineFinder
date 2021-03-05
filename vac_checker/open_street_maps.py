94cd8b0d91acc4a34acbe92f465e6a43610e78ad
Harmohan Gahlla <harmohangahlla@gmail.com>
2021-03-05 18:26:20 -0500

    Updated Open Street Maps

--- vac_checker/open_street_maps.py
+++ vac_checker/open_street_maps.py
@@ -4,7 +4,7 @@ from geopy.geocoders import Nominatim
 import datetime
 
 
-class MapsClient:
+class OSMClient:
     def __init__(self, src_lat, src_long, dest_lat, dest_long):
         self.src_lat = src_lat
         self.src_long = src_long
@@ -23,20 +23,18 @@ class MapsClient:
 
     def get_route(self):
         # Call the OSMR API
-        res = requests.get(
+        response = requests.get(
             f"http://router.project-osrm.org/route/v1/car/{self.src_long},{self.src_lat};{self.dest_long},{self.dest_lat}?overview=false""")
 
         # Load the response with the json library
         # By default you get only one alternative so you access 0-th element of the `routes`
-        routes = json.loads(res.content)
-        route_1 = routes.get("routes")[0]
-        self.route = route_1
-        return route_1
+        routes = json.loads(response.content)
+        self.route = routes.get("routes")[0]
+        return self.route
 
     def get_duration(self):
-        duration = datetime.timedelta(seconds=self.route["duration"])
-        self.duration = duration
-        return duration
+        self.duration = datetime.timedelta(seconds=self.route["duration"])
+        return self.duration
 
     def display_duration(self):
         t = str(self.duration).split(":")
@@ -47,6 +45,8 @@ class MapsClient:
             return t[1] + " min"
 
     def get_distance(self):
-        miles = round(self.route["distance"] * 0.000621371192, 2)
-        self.distance = miles
-        return miles
+        self.distance = round(self.route["distance"] * 0.000621371192, 2)
+        return self.distance
+
+    def display_distance(self):
+        return f"{self.distance} mi"
--- vac_checker/vac_checker.py
+++ vac_checker/vac_checker.py
@@ -1,7 +1,7 @@
 from requests_utils import get_json_data
 from database import Database
 from location import Location
-from travel import MapsClient
+from open_street_maps import OSMClient
 from notification import MessageClient
 import datetime
 import os
@@ -16,7 +16,7 @@ def populate_location_db(data):
     if count == 0:
         for loc in data['responsePayloadData']['data']['TX']:
             # Convert city name to latitude and longitude
-            lat, long = MapsClient.get_lat_long(loc['city'] + ', ' + loc['state'])
+            lat, long = OSMClient.get_lat_long(loc['city'] + ', ' + loc['state'])
             Location.save_to_db(loc['city'], loc['state'], lat, long, loc['status'])
             print(loc['city'] + ", " + loc['state'], lat, long, loc['status'])
 
@@ -31,26 +31,18 @@ def update_location_db(data):
 # Notify users of available locations within in certain drive time
 def get_locations_within_travel_time(source, locations, hr, m):
     msg_client = MessageClient()
-    lat, long = MapsClient.get_lat_long(source)
+    lat, long = OSMClient.get_lat_long(source)
     for loc in locations:
-        maps = MapsClient(lat, long, loc.latitude, loc.longitude)
+        maps = OSMClient(lat, long, loc.latitude, loc.longitude)
         maps.get_route()
 
-        if maps.get_duration() < datetime.timedelta(hours=hr, minutes=m):
-            msg = f"{loc.city}, {loc.state}\n{maps.display_duration()}\n{maps.get_distance()} miles\n"
+        if maps.get_duration() <= datetime.timedelta(hours=hr, minutes=m):
+            maps.get_distance()
+            msg = f"{loc.city}, {loc.state}\n{maps.display_duration()}\n{maps.display_distance()}\n"
             print(msg)
             notification = msg_client.send_notification(msg)
             print(f"Message Successfully Sent ({notification.sid})")
 
-
-# Calculate travel time and distance for all available locations
-def calculate_travel_info(source, locations):
-    lat, long = MapsClient.get_lat_long(source)
-    for loc in locations:
-        maps = MapsClient(lat, long, loc.latitude, loc.longitude)
-        maps.get_route()
-        maps.get_duration()
-        print(f"{loc.city}, {loc.state}\n{maps.display_duration()}\n{maps.get_distance()} miles\n")
 
 
 # Main function (Entry point for Google's Cloud Functions)
